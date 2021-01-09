85daca5316ac364b06bf443625b3281951f7a5c2
yuchen <ycx424>
2021-01-09 13:36:30 -0500

    lstm

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -54,7 +54,7 @@ class BasicMAC:
         return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
 
     def init_hidden(self, batch_size):
-        self.hidden_states = self.agent.init_hidden().unsqueeze(0).expand(batch_size, self.n_agents, -1)  # bav
+        self.hidden_states = [self.agent.init_hidden().unsqueeze(0).expand(batch_size, self.n_agents, -1)]*2  # bav
 
     def parameters(self):
         return self.agent.parameters()
--- src/modules/agents/rnn_agent.py
+++ src/modules/agents/rnn_agent.py
@@ -8,7 +8,7 @@ class RNNAgent(nn.Module):
         self.args = args
 
         self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
-        self.rnn = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
+        self.rnn = nn.LSTMCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
         self.fc2 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
 
     def init_hidden(self):
@@ -17,7 +17,8 @@ class RNNAgent(nn.Module):
 
     def forward(self, inputs, hidden_state):
         x = F.leaky_relu(self.fc1(inputs))
-        h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
-        h = self.rnn(x, h_in)
+        h_in = (hidden_state[0].reshape(-1, self.args.rnn_hidden_dim),
+                hidden_state[1].reshape(-1, self.args.rnn_hidden_dim))
+        h, c = self.rnn(x, h_in)
         q = self.fc2(h)
-        return q, h
+        return q, (h,c)
